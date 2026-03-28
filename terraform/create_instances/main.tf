4d9837008e449ee10d9ba062ab1e06c2c701d148
apapaniko <apapaniko@ote.gr>
2026-03-29 01:09:50 +0200

    feat: add health endpoint for load balancer checks

--- terraform/create_instances/main.tf
+++ terraform/create_instances/main.tf
@@ -177,15 +177,15 @@ resource "aws_lb_target_group" "app_tg" {
   protocol = "HTTP"
   vpc_id   = var.vpc_id
 
-  health_check {
-    path                = "/api/citizens"
-    port                = var.app_port
-    protocol            = "HTTP"
-    matcher             = "200"
-    healthy_threshold   = 3
-    unhealthy_threshold = 3
-    interval            = 30
-  }
+    health_check {
+      path                = "/health"
+      port                = var.app_port
+      protocol            = "HTTP"
+      matcher             = "200"
+      healthy_threshold   = 3
+      unhealthy_threshold = 3
+      interval            = 30
+    }
 }
 
 resource "aws_lb_target_group_attachment" "app_attach" {
